8e6882785df5f16c5870c5fa7ea9b53e889488c0
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-25 22:47:55 +0530

    Defined evaluation of the current session of the user and connected llm model and evaluationChain

--- server/src/services/evaluationService.js
+++ server/src/services/evaluationService.js
@@ -1,32 +1,43 @@
-import evaluationChain from "./evaluationChain"
-import Result from "../models/result";
-
+import evaluationChain from "./evaluationChain.js"
+import Result from "../models/Result.js";
+import { MODEL_USED } from "../config/groq.js";
 
 
 export const evaluateSession= async (session)=>{
     try{
+
+        const transcript= session.questions.map((item)=>`
+            Question: ${item.questionText}
+            Answer: ${item.answer?.transcript || "No Answer"}
+        `).join("\n");
+
 
         const response= await evaluationChain.invoke({
             role: session.role,
             interviewType: session.interviewType,
             difficulty: session.difficulty,
             experienceYears: session.experienceYears,
-            focusSkills: session.focusSkills,
-            transcript: session.questions,
+            focusSkills: session.focusSkills.join(", "),
+            transcript,
         });
 
-        const parsedResponse= JSON.parse(response.content);
 
-        await Result.create({
+        const result= await Result.create({
             session: session._id,
-            overallScore: parsedResponse.overallScore,
-            domainScore: parsedResponse.domainScore,
-            communicationScore: parsedResponse.communicationScore,
-            feedback: parsedResponse.feedback,
-            strengths: parsedResponse.strengths,
-            weaknesses: parsedResponse.weaknesses,
-            recommendations: parsedResponse.recommendations,
+            overallScore: response.overallScore,
+            domainScore: response.domainScore,
+            communicationScore: response.communicationScore,
+            feedback: response.feedback,
+            strengths: response.strengths,
+            weaknesses: response.weaknesses,
+            recommendations: response.recommendations,
+            modelUsed: MODEL_USED,
+            isPartialEvaluation: session.status==="abandoned",
         });
+
+        await result.populate("session");
+
+        return result;
 
     }catch(err){
         throw new Error(err.message);
