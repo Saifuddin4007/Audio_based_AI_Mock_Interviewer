00cf2a4e37e56494a26a1d3d87aa70bb2579cbbf
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-26 21:13:07 +0530

    Added redis connection and redis API call

---
+++ server/src/config/redis.js
@@ -0,0 +1,15 @@
+import Redis from 'ioredis';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const redisClient= new Redis(process.env.REDIS_URL);
+
+redisClient.on('connect', ()=>{
+    console.log('Redis Connected');
+})
+
+redisClient.on('error', (err)=>{
+    console.log('Redis Error: ', err);
+})
+
+export default redisClient;
