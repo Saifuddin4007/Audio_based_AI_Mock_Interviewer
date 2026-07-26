55fdcf7ca8b9856c9a2b710788a4daccacc0a063
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-26 21:12:12 +0530

    Added grqo llm model connection and API call

--- server/src/config/groq.js
+++ server/src/config/groq.js
@@ -1,10 +1,19 @@
-import { ChatGroq } from '@langchain/groq';
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+
+import { ChatGroq } from '@langchain/groq';
+
+export const MODEL_USED= "llama-3.3-70b-versatile";
+
 
 const llm= new ChatGroq({
    apiKey: process.env.GROQ_API_KEY,
-   model: "llama-3.3-70b-versatile",
+   model: MODEL_USED,
    temperature: 0.6,
    maxRetries: 2,
    maxTokens: 200 
