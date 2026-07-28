f466a489d0dbecbf8e4b5f13af47f7cc73275beb
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-28 20:18:51 +0530

    Added sessionRoutes in which all routes related to session are defined

--- server/src/routes/sessionRoutes.js
+++ server/src/routes/sessionRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { auth } from '../middlewares/auth';
-import { createSession } from '../controllers/sessionControllers';
+import { createSession, deleteOneSession, getALLSessions, getOneSession } from '../controllers/sessionControllers';
 
 
 const router= express.Router();
@@ -8,5 +8,8 @@ const router= express.Router();
 
 
 router.post('/', auth, createSession);
+router.get('/sessions/:sessionId', auth, getOneSession);
+router.get('/sessions', auth, getALLSessions);
+router.delete('/sessions/:sessionId', auth, deleteOneSession);
 
 export default router;
