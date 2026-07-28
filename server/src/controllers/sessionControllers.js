c19a43fe0a8a3c7446c50638ddec420ed1fa2f07
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-28 20:09:59 +0530

    Updated session controllers and added getAllSessions, getOneSession and deleteOneSession are defined

--- server/src/controllers/sessionControllers.js
+++ server/src/controllers/sessionControllers.js
@@ -25,3 +25,68 @@ export const createSession = async (req, res)=>{
         return res.status(500).json({error:err.message});
     }
 }
+
+
+
+export const getOneSession= async (req,res)=>{
+    try{
+        const { sessionId }= req.params;
+        if(!sessionId){
+            return res.status(400).json({message:"Unauthorized"});
+        }
+        const session= await Session.findById(sessionId);
+        if(!session){
+            return res.status(404).json({message:"Session not found"});
+        }
+
+        if(session.user.toString() !== req.userId){
+            return res.status(403).json({message:"Unauthorized User"});
+        }
+
+        return res.status(200).json({message:"Session found", session});
+
+    }catch(err){
+        return res.status(500).json({error:err.message});
+    }
+}
+
+
+export const getALLSessions= async (req,res)=>{
+    try{
+        const sessions= await Session.find({
+            user: req.userId
+        });
+        if(sessions.length===0){
+            return res.status(404).json({message:"No sessions found"});
+        }
+        return res.status(200).json({message:"All sessions", sessions});
+
+    }catch(err){
+        return res.status(500).json({error:err.message});
+    }
+}
+
+
+export const deleteOneSession= async (req,res)=>{
+    try{
+        const { sessionId }= req.params;
+        if(!sessionId){
+            return res.status(400).json({message:"Unauthorized"});
+        }
+
+        const session= await Session.findByIdAndDelete(sessionId);
+        if(!session){
+            return res.status(404).json({message:"Session not found"});
+        }
+
+        if(session.user.toString() !== req.userId){
+            return res.status(403).json({message:"Unauthorized User"});
+        }
+
+        return res.status(200).json({message:"Session deleted"});
+
+    }catch(err){
+        return res.status(500).json({error:err.message});   
+    }
+}
+
