654d6d164cb518ca4d97157b80104191bb5e55de
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-07-28 20:08:26 +0530

    Added result controllers in which getAllResults and getOneResult are defined

---
+++ server/src/controllers/resultControllers.js
@@ -0,0 +1,41 @@
+import Result from "../models/result";
+
+
+export const getAllResults= async (req,res)=>{
+    try{
+    
+        const results= await Result.find().populate({
+            path: "session",
+            match: {user: req.userId}
+        });
+        const userResults= results.filter(result=> result.session !== null);
+        if(userResults.length===0){
+            return res.status(404).json({message: "No results found"});
+        }
+        return res.status(200).json(userResults);
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
+}
+
+
+export const getOneResult= async(req,res)=>{
+    try{
+        const { sessionId }= req.params;
+
+        const result= await Result.findOne({
+            session: sessionId
+        }).populate("session");
+        if(!result){
+            return res.status(404).json({message: "No result found"});
+        }
+
+        if(result.session.user.toString() !== req.userId){
+            return res.status(403).json({message: "Unauthorized"})
+        }
+
+        return res.status(200).json(result);
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
+}
