c31114ff8ee9977b663572fa13a4075f7a9e68c5
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-08-05 11:30:15 +0530

    Added the rest of the routes like session, interview and result

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import dbConnection from './src/config/db.js';
 import authRoutes from './src/routes/authRoutes.js';
+import sessionRoutes from './src/routes/sessionRoutes.js';
+import interviewRoutes from './src/routes/interviewRoutes.js';
+import resultRoutes from './src/routes/resultRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
-
 
 
 const app= express();
@@ -16,12 +18,18 @@ app.use(cookieParser());   // must come before any route using req.cookies
 
 //Routes
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/session', sessionRoutes);
+app.use('/api/v1/interview', interviewRoutes);
+app.use('/api/v1/result', resultRoutes);
 
 
 
-const PORT= process.env.PORT || 4001;
+
+
+
+const PORT= process.env.PORT || 4001 ;
 
 dbConnection();
 app.listen(PORT, ()=>{
-    console.log("The server is started");
+    console.log(`The server is started at PORT:${PORT}`);
 })
