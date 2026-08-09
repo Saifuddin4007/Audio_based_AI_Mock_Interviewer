123995d7c287370742b61ecbee7afd7199bf291b
Saifuddin Khan <saifuddinkhan4007@gmail.com>
2026-08-09 23:14:13 +0530

    Added greet user component

---
+++ client/src/components/GreetUser.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GreetUser = () => {
+  return (
+    <>
+        <div>Hello {}, Welcome to the Mock Interviewer</div>
+    </>
+  )
+}
+
+export default GreetUser
